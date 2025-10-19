205229ed27bf8db78df9d1a3cff8ac6527c537af
amirpeyman <aptech.89113@gmail.com>
2025-10-19 22:27:19 +0330

    Add Day 6: Functions, Code Blocks and While Loops

---
+++ learning_100_days_of_code_with_angele_yu/day_06/task_01__functions.py
@@ -0,0 +1,27 @@
+# Functions 👇
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+# Defining a new function 👇
+def my_function():
+    print("Hello")
+    print("Bye")
+
+def get_user_name():
+    name = input("What is your name? ")
+    print("Hello, " + name)
+
+# Calling function 👇
+my_function()
+get_user_name()
+
+
+# While Loop 👇
+# while something_is_true:               # "something_is_true" is Particular Condition ✅
+#     # Do something repeatedly
+
+
+# Infinite Loop ==> It happens when condition never becomes 'False' 👇
+while 5 > 3:
+    print("Ok")
